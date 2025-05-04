ad8feb3a6a58f7abede2c0255b9759b61b6fdbba
Adamya-Gupta <adamyagupta.2011@gmail.com>
2025-05-04 23:25:08 +0530

    profile

--- app.js
+++ app.js
@@ -19,9 +19,10 @@ app.get('/login',(req,res)=>{
     res.render("login")    
 })
 
-app.get('/profile', isLoggedIn, (req,res)=>{
-    console.log(req.user)
-    res.render("login")
+app.get('/profile', isLoggedIn, async (req,res)=>{
+    let user = await userModel.findOne({email:req.user.email})
+    // console.log(user)
+    res.render("profile",{user})
 })
 
 app.post('/register',async (req,res)=>{
@@ -55,7 +56,7 @@ app.post('/login',async (req,res)=>{
         if(result) {
             let token = jwt.sign({email:email, userid: user._id},"secret")
             res.cookie("token",token)
-            res.status(200).send("you can login")
+            res.status(200).redirect("/profile")
         }
         else res.redirect("/login")
     })
@@ -67,7 +68,7 @@ app.get('/logout',(req,res)=>{
 })
 
 function isLoggedIn(req,res,next){
-    if(req.cookies.token === "") res.send("You must be logged in")
+    if(req.cookies.token === "") res.redirect("/login")
     else{
         let data = jwt.verify(req.cookies.token,"secret")
         req.user = data
